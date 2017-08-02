50420a18e0c56eac4c06614960c0c06c06ed9b46
JunaidGool <junaidgool@outlook.com>
2017-08-02 10:50:27 +0200

    added shoeData

--- shoes.js
+++ shoes.js
@@ -1,9 +1,30 @@
 module.exports = function(){
     
+    var shoesData = [
+        {brandName : 'Addidas',
+         color : 'Red',
+         price : 350,
+         in_stock : 5
+        },
+        {brandName : 'Nike',
+         color : 'Blue', 
+         price : 500,
+         in_stock : 8
+        },
+        {brandName : 'Puma',
+         color : 'White', 
+         price : 250,
+         in_stock : 10
+        },
+        
+    ];
+    
     const shoes = function(req, res){
-        res.send('List of all Shoes');
+        res.send(shoesData);
     };
     const brands = function(req, res){
+        
+        
         res.send('List all Brands'); 
     };
     const sizes = function(req, res){
