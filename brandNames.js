f944b0a420d3823f09cd37b3473ddc75f6bdd6fd
JunaidGool <junaidgool@outlook.com>
2017-08-03 11:35:53 +0200

    completed get routes

--- brandNames.js
+++ brandNames.js
@@ -1,19 +1,19 @@
-module.exports = function(shoesData){
-
-    var brandNames = [];
-    
-    
-    function allBrands(){
-        for (i=0;i<shoesData.length;i++){
-            var brandlist = shoesData[i].brandName;
-            brandNames.push(brandlist);
-        };
-        
-        return brandlist;
-    };
-    allBrands();
-    return brandNames;
-   
-}
+//module.exports = function(shoesData){
+//
+//    var brandNames = [];
+//    
+//    
+//    function allBrands(){
+//        for (i=0;i<shoesData.length;i++){
+//            var brandlist = shoesData[i].brandName;
+//            brandNames.push(brandlist);
+//        };
+//        
+//        return brandlist;
+//    };
+//    allBrands();
+//    return brandNames;
+//   
+//}
 
     
--- shoes.js
+++ shoes.js
@@ -2,49 +2,53 @@ module.exports = function(){
     //import shoesData from shoesData.js
     const ShoesData = require('./shoesData');
     //import BrandNames from brandNames.js
-    const BrandNames = require('./brandNames');
-    
+//    const BrandNames = require('./brandNames')
+
     //Stantiate ShoesData
     const shoesData = ShoesData();
     
-    //Stantiate BrandNames
-    const brandNames = BrandNames(shoesData);
+    //Stantiate brandNames
+//    const brandNames = BrandNames();
     
-    
-       
     const shoes = function(req, res){
         res.send(shoesData);
     };
-    
-    
+     
     const brands = function(req, res){
-        
         var selectedBrand = req.params.brandname;
         
         //loop through array
-        shoesData.find(function(shoesData){
-            if(shoesData.brandName == selectedBrand){
-                //stringify shoesData
-                var shoeString = (JSON.stringify(shoesData));
-                
-                res.send("You have selected brand: " + selectedBrand + shoeString);
-            }
-        
+        var foundBrands = shoesData.find(function(brand){
+            
+            return selectedBrand === brand.brandName;     
         });
+        
+        if (!foundBrands){
+            console.log("Invalid Brand")
+        }
+        
+        //stringify shoesData
+         var shoeString = (JSON.stringify(foundBrands));
+        
+        res.send("You have selected brand: " + selectedBrand + "" + shoeString);
     };
     
     
     const sizes = function(req, res){
         var selectedSize = req.params.size;
-
-        shoesData.find(function(shoesData){
-            if(shoesData.size == selectedSize){
-                //stringify shoesData
-                var shoeString = (JSON.stringify(shoesData));
-                
-               res.send("You have selected shoe size: " + selectedSize + shoeString);
-            }
+        // loop through array
+        var foundSizes = shoesData.find(function(size){
+            
+            return size.size == selectedSize;
         });  
+        
+        if(!foundSizes){
+            console.log("Invalid Size");
+        }
+        //stringify shoesData
+        var shoeString = (JSON.stringify(foundSizes));
+        
+        res.send("You have selected size: " + selectedSize + "" + shoeString)
     };
     
     const brandsAndSizes = function(req, res){
@@ -52,7 +56,11 @@ module.exports = function(){
         var selectedSize = req.params.size;
         
         shoesData.find(function(shoesData){
-            if(shoesData.brandName == selectedBrand && shoesData.size == selectedSize){
+            if (shoesData.brandName != selectedBrand && shoesData.size != selectedSize){
+                console.log("Invalid Selection");
+            } 
+            else if(shoesData.brandName == selectedBrand && shoesData.size == selectedSize){
+                
                 //stringify shoesData
                 var shoeString = (JSON.stringify(shoesData));
                 
